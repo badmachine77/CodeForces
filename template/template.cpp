2e9f503ec9d9701ad010953c511cb2937b597d09
Konstantin <konstantin.tomov@tum.de>
2026-05-15 22:57:41 +0200

    added first template

---
+++ template/template.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+void solve(){
+    
+}
+
+int main(){
+
+    ll t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+/*
+
+*/
